d4e97b65c0bdd8e1b7c3a9385e88a60442b0fd43
Alex Mihailov <ybahop@gmail.com>
2022-05-28 19:53:34 +0300

    fix: create project and burger

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ let cross = document.querySelector(".burger-menu__button");
 
 let burgMenu = document.querySelector(".burger-menu");
 
+let body = document.body;
+
+let active = document.querySelector(".burger-menu__nav-active");
+
+let burger = document.querySelector("#burger");
+
 menuBtn.addEventListener("click", function() {
     menu.classList.toggle("burger-menu__nav-active");
 });
@@ -18,6 +24,12 @@ menuBtn.addEventListener("click", function() {
     burgMenu.classList.toggle("burger-menu__active");
 });
 
-// menu.addEventListener("click", function() {
-//     burgMenu.classList.toggle("");
-// });
+body.addEventListener("click", function() {
+    body.classList.toggle("noscroll");
+});
+
+active.addEventListener("click", function() {
+    menu.classList.toggle("burger-menu__nav-active");
+    burgMenu.classList.toggle("burger-menu__active");
+    let menuBtn = document.querySelector(".burger-menu__button-active");
+});
